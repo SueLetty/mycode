800a8b71686a4eca60b4aa01f9cc112d2128101e
xiuxiang <xiuxiang1129@gmail.com>
2022-11-16 21:40:13 +0000

    copying local files with python

---
+++ challenges/modulechallenge.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# create a script that includes the trivia dictionary
+
+trivia= {
+         "category": "Entertainment: Film",
+         "type": "multiple",
+         "question": "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+         "correct_answer": "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+         "incorrect_answers": [
+             "&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+             "&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+             "&quot;Round up the usual suspects.&quot;"
+            ]
+        }
+
+# slice and print out the trivia question and the four answers( one correct, three incorrect) from the idctionary
+
+# use the html libaray to render the question/answers in the proper format
+
+
+# BONUS: have the user answer A, B, C, or D and see if they guess the answer correctly
+
+
---
+++ miniProject/miniProject1.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+# What is your Zodiac sign?
