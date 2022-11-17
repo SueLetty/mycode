b9f7b9b5ae2050ebe07742f892cd7a66ad43c89c
xiuxiang <xiuxiang1129@gmail.com>
2022-11-17 22:23:06 +0000

    fixed typo

--- miniProject/miniProject1.py
+++ miniProject/miniProject1.py
@@ -17,7 +17,7 @@ def yearToZodiac():
                 11:"Pig"}
 
     # print a question
-    print("Do you want to know your (somoone's) Chinese Zodiac sign?")
+    print("Do you want to know your or someone's Chinese Zodiac sign?")
     
     # set a condition
     condition = True
@@ -25,7 +25,7 @@ def yearToZodiac():
     while condition:
 
         # get a year from the user
-        year = input("Please type the year you (someone) was born(example: 2020): ")
+        year = input("Please type the year you or someone were/was born(example: 2020): ")
 
         # check user input validation
         if len(year) == 4 and year.isdigit():
@@ -39,7 +39,7 @@ def yearToZodiac():
         print()
 
         # ask user for keepgooing or not
-        condition = keepgoing()
+        condition = keepGoing()
  
 def zodiacToYear():
     year = {"Rat": 0, 
@@ -80,9 +80,9 @@ def zodiacToYear():
         print()
         
         # ask user for keepgooing or not
-        condition = keepgoing()
+        condition = keepGoing()
 
-def keepgoing():
+def keepGoing():
     condition = True
     # ask user for keepgooing or not
     userInput = input("Do you want to do it again?(y/n): ").lower()
@@ -104,8 +104,8 @@ def main():
     condition = True
     while condition:
         print("What do you want to check?")
-        print("\t1. using a year to check Chinese Zodiac")
-        print("\t2. using a Chinese Zodic to check years")
+        print("\t1. Enter a year to check your Chinese Zodiac")
+        print("\t2. Enter a Chinese Zodic to check which years correlate to which Chinese Zodiac")
         num = input(">")
         if num.isdigit():
             if num == "1":
